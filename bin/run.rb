9e807edcf4487746ed00cf95d23a5ddf61791b78
github username <github email address>
2019-05-30 15:01:28 -0400

    begin view_data method

--- bin/run.rb
+++ bin/run.rb
@@ -32,7 +32,7 @@ def get_student_name
 end
 
 def returning_student_selection(student)
-  data_choices = ["Join Band", "Drop Band", "Change Instrument", "Delete Profile"]
+  data_choices = ["Join Band", "Drop Band", "Change Instrument", "View Data", "Delete Profile"]
   data_choice = $prompt.select("Welcome back, #{student.name}! What would you like to do today?", data_choices)
   case data_choice
     when "Join Band"
@@ -49,6 +49,8 @@ def returning_student_selection(student)
     when "Change Instrument"
       instrument_selection("What instrument would you like to switch to?", student)
       puts "Your new instrument is #{Instrument.all.find{|inst| inst.id == student.instrument_id}.name}!"
+    when "View Data"
+      view_data
     when "Delete Profile"
       Student.all.delete(student)
   end
@@ -117,6 +119,46 @@ def leave_or_start_over(msg)
       exit
   end
 end
+
+def view_data
+  puts "viewing data"
+end
+
+# def view_data
+#   choice = $prompt.select("Which data would you like to view?", ['Band Data', 'Instrument Data'])
+#   case choice
+#     when "Band Data"
+#       band_choice = Band.all.find{|band| band.name == $prompt.select("Please choose a band whose data you would like to view", Band.all.map{|band| band.name})}
+#       data_choice = $prompt.select("What data would you like to view?", %w(Roster Instruments))
+#       case data_choice
+#         when "Roster"
+#           puts "Lineup for #{band_choice.name}:"
+#           band_choice.students.each{|student| puts "#{student.name}: #{student.instrument.name}"}
+#         when "Instruments"
+#           num_arr = []
+#           arr = band_choice.instruments.map do |inst|
+#             if arr.include?(inst.name)
+#               num_arr[arr.index(inst.name)] += 1
+#             else
+#               arr << inst.name
+#               num_arr << 1
+#             end
+#             puts "#{band_choice.name} currently has:"
+#             arr.each_with_index do |val, index|
+#               if num_arr[index] == 1
+#                 puts "#{num_arr[index]} student on #{val}"
+#               else
+#                 puts "#{num_arr[index]} students on #{val}"
+#               end
+#             end
+#           end
+#       end
+#     when "Instrument Data"
+#       # see how many students play same instrument
+#       # see what bands have student playing same instrument
+#       # see what bands are lacking current instrument player
+#   end
+# end
 
 get_student_name
 initial_user_nav
